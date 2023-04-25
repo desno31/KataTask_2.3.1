2d18ec4b96a1607fd77c43cb09a4ca563b65eb95
desno31 <desno31@gmail.com>
2023-04-25 13:54:59 +0300

    Task, Don!

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,12 +1,17 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static web.model.Car.getCarList;
 
 @Controller
 public class HelloController {
@@ -19,6 +24,12 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/cars")
+	public String printCarsList(@RequestParam(value = "count", required = false) Integer count, Model model) {
+		model.addAttribute("carsList", getCarList(count));
+		return "cars";
 	}
 	
 }
